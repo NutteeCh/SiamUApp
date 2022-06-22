c8c948c13a2b1076ba6059e1abadafbbdd0260f6
logacyz <nithiphat.cho@gmail.com>
2022-06-22 16:21:54 +0700

    Modify File_name

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'package:projectreportsiamu/screens/login_screen.dart';
 import 'package:projectreportsiamu/screens/profile_screen.dart';
 import 'package:projectreportsiamu/screens/register_screen.dart';
 import 'package:projectreportsiamu/screens/report_detail_screen.dart';
-import 'package:projectreportsiamu/screens/report_detail_screen_admin.dart';
+import 'package:projectreportsiamu/screens/report_detail_admin_screen.dart';
 import 'package:projectreportsiamu/screens/report_form_screen.dart';
 import 'package:projectreportsiamu/screens/report_list_screen.dart';
 import 'package:projectreportsiamu/screens/service_screen.dart';
