8b1b7bb92b5dd8a23e9bc7809106eb73f100377e
Anakkapoom Sriamphai <fluke.ankp@gmail.com>
2022-07-16 14:23:59 +0700

    update banner

--- lib/screens/setting_screen.dart
+++ lib/screens/setting_screen.dart
@@ -39,49 +39,49 @@ class _SettingScreenState extends State<SettingScreen> {
           SizedBox(
             height: 30,
           ),
-          InkWell(
-            onTap: () {
-              // Navigator.of(context).pushNamed("/");
-            },
-            child: Container(
-              height: 50,
-              width: 350,
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 3,
-                      blurRadius: 7,
-                      offset: Offset(0, 3), // changes position of shadow
-                    ),
-                  ],
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(15),
-                      bottomRight: Radius.circular(15))),
-              child: Row(
-                children: [
-                  Text(
-                    "เกี่ยวกับเรา",
-                    style: TextStyle(
-                      color: Color(0xFF616161),
-                      fontSize: 18,
-                    ),
-                  ),
-                  Spacer(),
-                  Icon(
-                    Icons.arrow_forward_ios,
-                    color: SiamColors.grey,
-                    size: 20,
-                  )
-                ],
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
+          // InkWell(
+          //   onTap: () {
+          //     // Navigator.of(context).pushNamed("/");
+          //   },
+          //   child: Container(
+          //     height: 50,
+          //     width: 350,
+          //     padding: EdgeInsets.all(10),
+          //     decoration: BoxDecoration(
+          //         boxShadow: [
+          //           BoxShadow(
+          //             color: Colors.grey.withOpacity(0.5),
+          //             spreadRadius: 3,
+          //             blurRadius: 7,
+          //             offset: Offset(0, 3), // changes position of shadow
+          //           ),
+          //         ],
+          //         color: Colors.white,
+          //         borderRadius: BorderRadius.only(
+          //             topRight: Radius.circular(15),
+          //             bottomRight: Radius.circular(15))),
+          //     child: Row(
+          //       children: [
+          //         Text(
+          //           "เกี่ยวกับเรา",
+          //           style: TextStyle(
+          //             color: Color(0xFF616161),
+          //             fontSize: 18,
+          //           ),
+          //         ),
+          //         Spacer(),
+          //         Icon(
+          //           Icons.arrow_forward_ios,
+          //           color: SiamColors.grey,
+          //           size: 20,
+          //         )
+          //       ],
+          //     ),
+          //   ),
+          // ),
+          // SizedBox(
+          //   height: 20,
+          // ),
           InkWell(
             onTap: () async {
               await FirebaseAuth.instance.signOut();
